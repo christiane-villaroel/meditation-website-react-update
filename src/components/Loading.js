cc6b8a35dbeef673babd95f2cb489e4023a4739d
Christiane Villaroel <christianevillaroel@gmail.com>
2023-02-16 18:08:20 -0500

    Loading Component: Reactstrap Loading Animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,12 @@
+import { Spinner } from "reactstrap";
+
 const Loading = () => {
     return (
     <div className="d-flex justify-content-center">
-        <h3 className="text-align-center">...Loading</h3>
+         <Spinner animation="border" variant="secondary" role="status"/>
+         <h3 className="text-align-center">
+            Loading
+        </h3>
     </div>)
 };
 
