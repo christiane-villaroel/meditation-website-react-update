c6f0c1f54bf4c14ccc7cea6ecd68220705a91ebe
Christiane Villaroel <christianevillaroel@gmail.com>
2023-02-24 17:07:49 -0500

    DB.Json: img file path updates

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,3 @@
-/* export const localhostUrl = `http://localhost:8000/`;
-export const GitHubUrl = `https://my-json-server.typicode.com/christiane-villaroel/meditation-website-react-update/`; */
-//export const baseUrl = `https://meditation-project-server.herokuapp.com/`
+//export const localhostUrl = `http://localhost:8000/`;
+
 export const baseUrl =  `http://localhost:8000/`
