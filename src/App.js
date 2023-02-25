c0a54ee62a946256ae442ab2ff1f9323428f6f37
Christiane Villaroel <christianevillaroel@gmail.com>
2023-02-25 17:15:11 -0500

    Error Routes added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
 import AboutUsPage from './pages/AboutUsPage';
+import Error from './components/Error';
 function App() {
   
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Homepage />}/>
         <Route path='about'  element = {<AboutUsPage/>}/>
+        <Route path='*' element= {<Error/>}/>
       </Routes>
       <Footer/>
     </div>
