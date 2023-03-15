ddc353d2b8d15dbd1bb2bd5168f7d4271aa10d7f
Christiane Villaroel <christianevillaroel@gmail.com>
2023-03-15 10:36:55 -0400

    deploying backend

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+/* const express = require('express');
 const path = require('path');
 const jsonServer = require('json-server');
+const server = require('./data/db.json')
 const app = express();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('./data/db.json');
+const router = jsonServer.router(server);
 const port = process.env.PORT || 8000;
 
 app.use('/', middlewares, router);
@@ -16,3 +17,33 @@ app.get('/*', function (req, res) {
 app.listen(port,()=>{
     console.log('JSON Server us running')
 });
+ */
+/* const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port); */
+
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
