5d75d09ad39464ebfece017aff313b85e139cd96
Christiane Villaroel <christianevillaroel@gmail.com>
2023-03-17 15:32:49 -0400

    server.js

--- server.js
+++ server.js
@@ -29,22 +29,21 @@ server.use(router);
 
 server.listen(port); */
 
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults;
+const router = jsonServer.router('db.json')
+const port = process.env.PORT || 8000;
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build')));
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-server.listen(port,host, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+server.listen(port, ()=>{
+  console.log(`server is running on port ${port}`)
+});
