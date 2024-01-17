53e511e4588dd558bb9b1be402e2242bdaa11fc0
Christiane <christianevillaroel@gmail.com>
2024-01-16 20:08:01 -0500

    BaseURL.js: URL link update

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,4 @@
 //export const localhostUrl = `http://localhost:8000/`;
 //https://meditation-project-server.herokuapp.com/
 
-export const baseUrl =  `https://meditation-project-server.herokuapp.com/`
+export const baseUrl =  `https://git.heroku.com/meditation-website-json-server.git`
